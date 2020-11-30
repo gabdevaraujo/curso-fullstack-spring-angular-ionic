0b89903454e5cd849083d3d9596ac43d6a3fe60d
Gabriel <aqua.camus@hotmail.com>
2020-11-30 16:51:56 -0300

    Insert Categoria

--- src/main/java/com/gva/cursomcd/resource/CategoriaResource.java
+++ src/main/java/com/gva/cursomcd/resource/CategoriaResource.java
@@ -1,5 +1,9 @@
 package com.gva.cursomcd.resource;
 
+import java.net.URI;
+
+import javax.xml.ws.Response;
+
 import com.gva.cursomcd.domain.Categoria;
 import com.gva.cursomcd.service.CategoriaService;
 
@@ -7,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categorias")
@@ -26,6 +33,18 @@ public class CategoriaResource {
             return ResponseEntity.ok().body(categoria);
         }
         else return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(obj.getId())
+            .toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
 }
