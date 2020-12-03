4f6cf5bc8d1fa000aca568e4e616b0097f10d467
Gabriel <aqua.camus@hotmail.com>
2020-12-03 12:49:22 -0300

    Validação customizada de CPF ou CNPJ Na inserção de cliente

--- src/main/java/com/gva/cursomcd/dto/ClienteNewDTO.java
+++ src/main/java/com/gva/cursomcd/dto/ClienteNewDTO.java
@@ -2,34 +2,51 @@ package com.gva.cursomcd.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.gva.cursomcd.service.validation.ClienteInsert;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter @Setter
 @NoArgsConstructor
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @NotBlank(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "Tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
+    @NotBlank(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
     private String email;
 
     private String cpfOuCnpj;
 
     private Integer tipo;
 
+    @NotBlank(message = "Preenchimento obrigatório")
     private String logradouro;
 
+    @NotBlank(message = "Preenchimento obrigatório")
     private String numero;
 
     private String complemento;
 
     private String bairro;
 
+    @NotBlank(message = "Preenchimento obrigatório")
     private String cep;
 
+    @NotBlank(message = "Preenchimento obrigatório")
     private String telefone1;
 
     private String telefone2;
